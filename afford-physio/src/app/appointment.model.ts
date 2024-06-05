a6a46ae6f43a8d6f7aefdff7795930aa8267241e
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2024-06-05 22:47:05 +0200

    created appointment model

---
+++ afford-physio/src/app/appointment.model.ts
@@ -0,0 +1,35 @@
+import { Patient } from './patient.model';
+import { Physio } from './physio.model';
+
+export class Appointment {
+  constructor(
+    private _id: number,
+    private _patient: Patient,
+    private _physio: Physio,
+    private _location: string,
+    private _date: Date,
+    private _status: string
+  ) {}
+
+  public get id() {
+    return this._id;
+  }
+  public get patient() {
+    return this._patient;
+  }
+  public get physio() {
+    return this._physio;
+  }
+  public get location() {
+    return this._location;
+  }
+  public get date() {
+    return this._date;
+  }
+  public get status() {
+    return this._status;
+  }
+  public set status(status: string) {
+    this._status = status;
+  }
+}
