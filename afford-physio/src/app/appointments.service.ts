5ae3f016d7af436cd4ce76449fbed34965821f68
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2024-06-07 18:22:06 +0200

    filtered appointments by currect user id

--- afford-physio/src/app/appointments.service.ts
+++ afford-physio/src/app/appointments.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@angular/core';
 import { Appointment } from './appointment.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Patient } from './patient.model';
+import { Physio } from './physio.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppointmentsService {
-  public appointments: Appointment[] = [];
+  public appointments: Appointment[] = [
+    new Appointment(
+      349,
+      new Patient(2810128, 'name', 'email', 'password'),
+      new Physio(10491, 'name', 'spec', 'email', 'password'),
+      'locationtest',
+      'datetimetest',
+      'statuuuss'
+    ),
+  ];
   public appointmentsUpdated = new Subject<Appointment[]>();
   public appointmendId = 1;
   private jsonFilePath = 'assets/mockData/appointments.json';
@@ -27,5 +38,9 @@ export class AppointmentsService {
     if (index !== -1) {
       this.appointments[index] = updatedAppointment;
     }
+  }
+
+  getAppointments() {
+    return [...this.appointments];
   }
 }
--- afford-physio/src/app/appointments/appointments.page.ts
+++ afford-physio/src/app/appointments/appointments.page.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Appointment } from '../appointment.model';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { AppointmentsService } from '../appointments.service';
+import { Patient } from '../patient.model';
+import { Physio } from '../physio.model';
 
 @Component({
   selector: 'app-appointments',
   templateUrl: 'appointments.page.html',
   styleUrls: ['appointments.page.scss'],
 })
-export class AppointmentsPage {
-  constructor() {}
+export class AppointmentsPage implements OnInit {
+  appointments: Appointment[] = [];
+
+  constructor(
+    public authService: AuthService,
+    public appointmentsService: AppointmentsService
+  ) {}
+
+  ngOnInit(): void {
+    this.appointments = this.appointmentsService
+      .getAppointments()
+      .filter((appt) => {
+        if (appt.patient.id === this.authService.currentUser.id) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    console.log(this.appointments);
+  }
 }
